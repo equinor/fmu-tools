404dd84cb145acaa44f83941b1195ab6586bc3fd
Trine Alsos <tral@statoil.com>
2018-09-26 14:45:25 +0200

    Changes to plot in webviz instead of webportal

--- examples/webviz_main.py
+++ examples/webviz_main.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Loading nested configs for FMU"""
+"""
+before running, currently using:
+source /project/res/komodo/testing/enable
+export PYTHONPATH=/project/res/webviz_multiuse/webviz_20180919/:$PYTHONPATH
+"""
 from __future__ import division, print_function, absolute_import
-from fmu.tools import sensitivities as sens
-from webportal import Webportal
+from fmu.tools.sensitivities import add_webviz_tornadoplots
+from webviz import Webviz
 
 
-html_foldername = './webportal_example'
+html_foldername = './webviz_example'
 title = 'Snorreberg'
 
-web = Webportal(title)
+web = Webviz(title, theme='equinor')
 configpath = '../tests/data/sensitivities/config/'
 
 # add different types of plots to webportal
-sens.add_webviz_tornadoplots(web, configpath +
-                             'config_example_geovolume_ensemble.yaml')
-sens.add_webviz_tornadoplots(web, configpath +
-                             'config_example_geovolume.yaml')
-sens.add_webviz_tornadoplots(web, configpath +
-                             'config_example_eclipse.yaml')
+add_webviz_tornadoplots(web, configpath +
+                         'config_example_geovolume_ensemble.yaml')
+add_webviz_tornadoplots(web, configpath +
+                         'config_example_geovolume.yaml')
+add_webviz_tornadoplots(web, configpath +
+                         'config_example_eclipse.yaml')
 
 # Finally, write html
 web.write_html(html_foldername, overwrite=True, display=True)
