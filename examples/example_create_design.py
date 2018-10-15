60bc779801969a3f4f03359ae0771f2c8c1ad016
Trine Alsos <tral@statoil.com>
2018-10-15 16:23:51 +0200

    Added first version of code for automatic generation of design matrices

---
+++ examples/example_create_design.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Example: generation of design matrix
+"""
+from __future__ import division, print_function, absolute_import
+from fmu.tools.sensitivities import DesignMatrix
+from fmu.config import oyaml as yaml
+
+design_configfile = '../tests/data/sensitivities/config/config_design_input.yaml'
+
+with open(design_configfile) as input_file:
+    input_dict=yaml.load(input_file)
+
+design1 = DesignMatrix()
+design1.set_defaultvalues(input_dict['defaultvalues'])
+design1.generate(input_dict)
+print(design1.designvalues)
+design1.to_csv('design.xlsx')
---
+++ tests/test_create_design.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""Testing code for generation of design matrices"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+from fmu import config
+from fmu.config import oyaml as yaml
+from fmu.tools.sensitivities import DesignMatrix
+
+fmux = config.etc.Interaction()
+logger = fmux.basiclogger(__name__)
+
+if not fmux.testsetup():
+    raise SystemExit()
+
+
+def test_generate_onebyone():
+    """Test generation of onebyone design"""
+
+    if '__file__' in globals():
+        # Easen up copying test code into interactive sessions
+        testdir = os.path.dirname(os.path.abspath(__file__))
+    else:
+        testdir = os.path.abspath('.')
+
+    with open(
+            testdir +
+            '/data/sensitivities/config/' +
+            'config_design_input.yaml') as input_file:
+        input_dict = yaml.load(input_file)
+
+    design = DesignMatrix()
+    design.set_defaultvalues(input_dict['defaultvalues'])
+    design.generate(input_dict)
+
+    # Checking dimensions of design matrix
+    assert design.designvalues.shape == (80, 10)
+
+    # Add more tests...
