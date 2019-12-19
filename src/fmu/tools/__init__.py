109f214e0121f9d0001940383110f39d5c6fb4c6
Trine Alsos <tral@statoil.com>
2019-12-19 13:16:02 +0100

    Minor changes for python 3

--- src/fmu/tools/__init__.py
+++ src/fmu/tools/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for fmu_tools"""
+import logging
 
 from ._theversion import theversion
 __version__ = theversion()
@@ -13,5 +14,9 @@ from .sensitivities import DesignMatrix # noqa
 from .sensitivities import summarize_design # noqa
 from .sensitivities import calc_tornadoinput # noqa
 from .sensitivities import find_combinations # noqa
-from .sensitivities import add_webviz_tornadoplots # noqa
 from .sensitivities import excel2dict_design # noqa
+
+try:
+    from .sensitivities import add_webviz_tornadoplots # noqa
+except ImportError:
+    logging.warning("Webviz not found, cannot make Tornado charts")
