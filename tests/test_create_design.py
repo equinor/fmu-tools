6f91b087452922033dfd1da2e0da5689db20a134
Håvard Berland <havb@equinor.com>
2020-03-13 15:31:00 +0100

    Make fmu.tools independent of fmu.config (#17)
    oyaml replaced by yaml

--- tests/test_create_design.py
+++ tests/test_create_design.py
@@ -8,14 +8,7 @@ from __future__ import print_function
 import os
 import pandas as pd
 
-from fmu import config
 from fmu.tools.sensitivities import DesignMatrix, excel2dict_design
-
-fmux = config.etc.Interaction()
-logger = fmux.basiclogger(__name__)
-
-if not fmux.testsetup():
-    raise SystemExit()
 
 
 def test_generate_onebyone(tmpdir):
