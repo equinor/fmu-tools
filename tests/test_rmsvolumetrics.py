7513de22038646e7609a8a610ec911efe9da074a
Håvard Berland <havb@equinor.com>
2020-03-13 14:46:18 +0100

    Blacken and linting of volumetrics code (#14)

--- tests/test_rmsvolumetrics.py
+++ tests/test_rmsvolumetrics.py
@@ -8,52 +8,48 @@ from __future__ import print_function
 import os
 import glob
 
-from fmu import config
 from fmu.tools.rms import volumetrics
-
-fmux = config.etc.Interaction()
-logger = fmux.basiclogger(__name__)
-
-if not fmux.testsetup():
-    raise SystemExit()
 
 
 def test_volumetrics():
     """Test parsing of many real examples from RMS"""
 
-    if '__file__' in globals():
+    if "__file__" in globals():
         # Easen up copying test code into interactive sessions
         testdir = os.path.dirname(os.path.abspath(__file__))
     else:
-        testdir = os.path.abspath('.')
+        testdir = os.path.abspath(".")
 
-    volfiles = glob.glob(testdir + '/data/rmsvolumetrics/*txt')
+    volfiles = glob.glob(testdir + "/data/rmsvolumetrics/*txt")
 
     for file in volfiles:
-        df = volumetrics.rmsvolumetrics_txt2df(file)
+        dframe = volumetrics.rmsvolumetrics_txt2df(file)
 
         # Check that we did get some data:
-        assert len(df) > 0
+        assert len(dframe) > 0
 
         # Check for no non-uppercase column names:
-        collist = list(df.columns)
+        collist = list(dframe.columns)
         assert [x.upper() for x in collist] == collist
 
         # Check for no string 'Totals' in any cell
-        assert 'Totals' not in df.to_string()
+        assert "Totals" not in dframe.to_string()
 
     # Test zone renamer:
-    def myrenamer(s):
-        return s.replace('Larsson', 'E')
-    df = volumetrics.rmsvolumetrics_txt2df(testdir + '/data/rmsvolumetrics/' +
-                                           '14_geo_gas_1.txt',
-                                           zonerenamer=myrenamer)
-    assert 'Larsson' not in df.to_string()
-    assert 'E3_1' in df.to_string()
+    def myrenamer(a_zone):
+        """Callback function for zone renaming"""
+        return a_zone.replace("Larsson", "E")
+
+    dframe = volumetrics.rmsvolumetrics_txt2df(
+        testdir + "/data/rmsvolumetrics/" + "14_geo_gas_1.txt", zonerenamer=myrenamer
+    )
+    assert "Larsson" not in dframe.to_string()
+    assert "E3_1" in dframe.to_string()
 
     # Test columnrenamer:
-    columnrenamer = {'Region index': 'FAULTSEGMENT'}  # this will override
-    df = volumetrics.rmsvolumetrics_txt2df(testdir + '/data/rmsvolumetrics/' +
-                                           '14_geo_gas_1.txt',
-                                           columnrenamer=columnrenamer)
-    assert 'FAULTSEGMENT' in df.columns
+    columnrenamer = {"Region index": "FAULTSEGMENT"}  # this will override
+    dframe = volumetrics.rmsvolumetrics_txt2df(
+        testdir + "/data/rmsvolumetrics/" + "14_geo_gas_1.txt",
+        columnrenamer=columnrenamer,
+    )
+    assert "FAULTSEGMENT" in dframe.columns
