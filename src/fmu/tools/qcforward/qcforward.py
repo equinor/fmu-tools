aec0e937afb1906726f0543a7d6a79eb772caf67
Håvard Berland <havb@equinor.com>
2020-04-14 10:40:06 +0200

    Blacken everything (#31)

--- src/fmu/tools/qcforward/qcforward.py
+++ src/fmu/tools/qcforward/qcforward.py
@@ -1,18 +1,10 @@
-
-
 class QCForward(object):
-
     def __init__(self):
         self._name = None
         self._whatever = None
 
-
     def wellzonation_vs_grid(
-        self,
-        gridfile=None,
-        zonefile=None,
-        rmsproject=None,
-        gridname=None
+        self, gridfile=None, zonefile=None, rmsproject=None, gridname=None
     ):
         """Check well zonation or perforations vs 3D grid."""
         print("Testing")
