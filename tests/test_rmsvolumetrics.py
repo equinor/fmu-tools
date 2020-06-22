e0693e989ab39aeb5c31f862bafcd1080c88816a
Håvard Berland <havb@equinor.com>
2020-06-22 09:15:56 +0200

    New command line client, rmsvolumetrics2csv (#43)
    Set to replace widespread script in various FMU projects with same name
    
    Behaviour is slightly changed from the 2014 version, in column names
    and there is no handling of realization names here.

--- tests/test_rmsvolumetrics.py
+++ tests/test_rmsvolumetrics.py
@@ -6,7 +6,13 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import sys
 import glob
+import subprocess
+
+import pytest
+
+import pandas as pd
 
 from fmu.tools.rms import volumetrics
 
@@ -53,3 +59,63 @@ def test_volumetrics():
         columnrenamer=columnrenamer,
     )
     assert "FAULTSEGMENT" in dframe.columns
+
+
+@pytest.mark.integration
+def test_commandlineclient_installed():
+    """Test endpoint is installed"""
+    assert subprocess.check_output(["rmsvolumetrics2csv", "-h"])
+
+
+@pytest.mark.integration
+def test_commandlineclient(tmpdir):
+    """Test endpoint"""
+
+    testdir = os.path.dirname(os.path.abspath(__file__))
+    tmpdir.chdir()
+
+    sys.argv = [
+        "rmsvolumetrics2csv",
+        os.path.join(testdir, "data/rmsvolumetrics/1_geogrid_vol_oil_1.txt"),
+        "--output",
+        "geogrid_oil.csv",
+    ]
+    volumetrics.rmsvolumetrics2csv_main()
+
+    disk_df = pd.read_csv("geogrid_oil.csv")
+    assert "STOIIP_OIL" in disk_df
+
+    sys.argv = [
+        "rmsvolumetrics2csv",
+        os.path.join(testdir, "data/rmsvolumetrics/14_geo_gas_1.txt"),
+        "--output",
+        "geo_gas.csv",
+    ]
+    volumetrics.rmsvolumetrics2csv_main()
+    disk_df = pd.read_csv("geo_gas.csv")
+    assert "GIIP_GAS" in disk_df
+
+    # Test that --phase will override the inferral
+
+    sys.argv = [
+        "rmsvolumetrics2csv",
+        os.path.join(testdir, "data/rmsvolumetrics/14_geo_gas_1.txt"),
+        "--output",
+        "geo_foophase.csv",
+        "--phase",
+        "FOOBAR",
+    ]
+    volumetrics.rmsvolumetrics2csv_main()
+    disk_df = pd.read_csv("geo_foophase.csv")
+    assert "GIIP_FOOBAR" in disk_df
+
+    # Test that parent directories will be created for output
+    sys.argv = [
+        "rmsvolumetrics2csv",
+        os.path.join(testdir, "data/rmsvolumetrics/14_geo_gas_1.txt"),
+        "--output",
+        "foo/bar/com.csv",
+    ]
+    volumetrics.rmsvolumetrics2csv_main()
+    disk_df = pd.read_csv("foo/bar/com.csv")
+    assert "GIIP_GAS" in disk_df
