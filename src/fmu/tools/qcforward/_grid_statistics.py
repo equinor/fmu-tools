ab058df2335f83675cf31f5079f682535fb19008
Jan C. Rivenæs <jan@rivenaes.net>
2020-07-06 20:17:58 +0200

    wip: docs on qcforward

---
+++ src/fmu/tools/qcforward/_grid_statistics.py
@@ -0,0 +1,11 @@
+"""
+This private module in qcforward is used for grid statistics
+"""
+
+import xtgeo
+from . import _parse_data
+
+
+def grid_statistics(self, data):
+
+    _parse_data.parse(self, data)
