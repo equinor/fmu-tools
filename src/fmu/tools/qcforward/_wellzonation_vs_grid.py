89562def41be6eefdcba1e9f638d3e2465660806
Jan C. Rivenæs <jan@rivenaes.net>
2020-08-18 12:43:02 +0200

    wip: add perf and refactor

--- src/fmu/tools/qcforward/_wellzonation_vs_grid.py
+++ src/fmu/tools/qcforward/_wellzonation_vs_grid.py
@@ -1,9 +1,8 @@
 """
-This private module in qcforward is used to check wellzonation vs grid
+This private module in qcforward is used to check wellzonation vs grid zonation
 """
 from __future__ import absolute_import, division, print_function  # PY2
 
-import logging
 import sys
 from os.path import join
 import collections
@@ -11,26 +10,36 @@ import numpy as np
 import pandas as pd
 from . import _parse_data
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.CRITICAL)
-
+# named tuple for local data
 WZong = collections.namedtuple(
-    "WZong", "zonelogrange depthrange actions_each actions_all report"
+    "WZong",
+    "zonelogname zonelogrange depthrange actions_each actions_all "
+    "report perflogname perflogrange",
 )
 
 
-def _parse_wzong(data):
-    """Parse ande check data local to this routine, return a named tuple"""
+def _parse_local_data(data):
+    """
+    Parse and check data which are more special/local to this routine, return
+    data via a named tuple
+
+    Args:
+        data (dict): The data dictionary
+    """
 
     # defaults:
+    zonelogname = "Zonelog"
     zonelogrange = (1, 99)
     depthrange = (0, 9999)
     actions_each = {"warnthreshold": 99, "stopthreshold": 50}
     actions_all = {"warnthreshold": 99, "stopthreshold": 88}
     report = {"file": None, "write": "write"}
+    perflogname = None
+    perflogrange = (1, 9999)
 
-    if "zonelogrange" in data:
-        zlrange = data["zonelogrange"]
+    if "zonelog" in data:
+        zonelogname = data["zonelog"].get("name", "Zonelog")
+        zlrange = data["zonelog"].get("range", zonelogrange)
         if (
             isinstance(zlrange, list)
             and len(zlrange) == 2
@@ -41,6 +50,8 @@ def _parse_wzong(data):
             zonelogrange = tuple(zlrange)
         else:
             raise ValueError("zonelogrange on wrong format: ", zlrange)
+    else:
+        raise ValueError("Key zonelog is missing in data")
 
     if "depthrange" in data:
         drange = data["depthrange"]
@@ -55,6 +66,10 @@ def _parse_wzong(data):
         else:
             raise ValueError("depthrange on wrong format: ", drange)
 
+    if "perforationlog" in data:
+        perflogname = data["perforationlog"].get("name", "PERFLOG")
+        perflogrange = tuple(data["perforationlog"].get("range", [1, 9999]))
+
     if "actions_each" in data:
         # todo check data
         actions_each = data["actions_each"]
@@ -68,29 +83,32 @@ def _parse_wzong(data):
         report = data["report"]
 
     wzong = WZong(
+        zonelogname=zonelogname,
         zonelogrange=zonelogrange,
         depthrange=depthrange,
         actions_each=actions_each,
         actions_all=actions_all,
         report=report,
+        perflogname=perflogname,
+        perflogrange=perflogrange,
     )
 
     return wzong
 
 
-def _make_report(
-    self, wzong, data, well_all, match_all, well_warn, well_stop, well_status
-):
-    """Make a report which e.g. can be used in webviz plotting"""
+def _make_report(self, wzong, results):
+    """Make a report which e.g. can be used in webviz plotting
 
-    res = collections.OrderedDict()
-    res["WELL"] = well_all
-    res["MATCH"] = match_all
-    res["WARN_LIMIT"] = well_warn
-    res["STOP_LIMIT"] = well_stop
-    res["STATUS"] = well_status
+    Args:
+        self (instance): The QCForward instance
+        wzong (named tuple): local data
+        results (dict): Results table
 
-    dfr = pd.DataFrame(res)
+    Returns:
+        A Pandas dataframe
+    """
+
+    dfr = pd.DataFrame(results)
 
     if wzong.report["file"]:
         reportfile = join(self._path, wzong.report["file"])
@@ -102,26 +120,26 @@ def _make_report(
     return dfr
 
 
-def wellzonation_vs_grid(
-    self, data
-):  # pylint: disable=too-many-locals, too-many-statements
+def _evaluate_per_well(self, wzong, inresults):
+    """Do a check per well"""
 
-    # parsing data stored is self._xxx (general data like grid)
-    self.print_info("Parsing data...")
-    _parse_data.parse(self, data)
-
-    # parse data that are special for this check
-    self.print_info("Parsing additional data...")
-    wzong = _parse_wzong(data)
-
-    match_all = []
-    well_all = []
-    well_warn_limit = []
-    well_stop_limit = []
-    well_status = []
+    results = inresults.copy()
 
     for wll in self._wells.wells:
         self.print_debug("Working with well {}".format(wll.name))
+
+        dfr = wll.dataframe
+        useperflog = "PERF__local"
+        if wzong.perflogname and wzong.perflogname in dfr.columns:
+            rng_min = wzong.perflogrange[0]
+            rng_max = wzong.perflogrange[1]
+            dfr[useperflog] = dfr[wzong.perflogname] * 0
+            dfr[useperflog].where(
+                (dfr[wzong.perflogname] >= rng_min)
+                & (dfr[wzong.perflogname] <= rng_max),
+                1,
+                inplace=True,
+            )
 
         res = self._grid.report_zone_mismatch(
             well=wll,
@@ -129,11 +147,12 @@ def wellzonation_vs_grid(
             zoneprop=self._gridzone,
             zonelogrange=wzong.zonelogrange,
             depthrange=wzong.depthrange,
+            perflogname=useperflog,
             resultformat=2,
         )
         self.print_debug(res)
 
-        well_all.append(wll.name)
+        results["WELL"].append(wll.name)
 
         if res:
             wname = wll.name
@@ -141,8 +160,8 @@ def wellzonation_vs_grid(
             self.print_info("Well: {0:30s} - {1: 5.3f}".format(wname, match))
             wlimit = wzong.actions_each["warnthreshold"]
             slimit = wzong.actions_each["stopthreshold"]
-            well_warn_limit.append(wlimit)
-            well_stop_limit.append(slimit)
+            results["WARN_LIMIT"].append(wlimit)
+            results["STOP_LIMIT"].append(slimit)
 
             status = "OK"
             if match < wlimit:
@@ -150,24 +169,59 @@ def wellzonation_vs_grid(
             if match < slimit:
                 status = "STOP"
 
-            match_all.append(match)
-            well_status.append(status)
+            results["MATCH"].append(match)
+            results["STATUS"].append(status)
+
         else:
-            match_all.append(0.0)
-            well_warn_limit.append("?")
-            well_stop_limit.append("?")
-            well_status.append("?")
+            results["MATCH"].append(float("nan"))
+            results["WARN_LIMIT"].append(float("nan"))
+            results["STOP_LIMIT"].append(float("nan"))
+            results["STATUS"].append(float("nan"))
+
+    return results
+
+
+def wellzonation_vs_grid(self, data):
+    """Main routine for evaulating well zonation match in 3D grids.
+
+    The routine depends on existing XTGeo functions for this purpose
+
+    Args:
+        data (dict): Input data
+    """
+
+    # parsing data stored is self._xxx (general data like grid)
+    self.print_info("Parsing general data...")
+    _parse_data.parse(self, data)
+
+    # parse data that are special for this check
+    self.print_info("Parsing additional data...")
+    wzong = _parse_local_data(data)
+
+    # results are stored in a dict based table which be turned into a Pandas
+    # dataframe in the end (most efficient to increment lists; then turn into pandas)
+    results = collections.OrderedDict(
+        [
+            ("WELL", []),
+            ("MATCH", []),
+            ("WARN_LIMIT", []),
+            ("STOP_LIMIT", []),
+            ("STATUS", []),
+        ]
+    )
+
+    results = _evaluate_per_well(self, wzong, results)
 
     # all data (look at averages)
-    match_allv = np.array(match_all)
+    match_allv = np.array(results["MATCH"])
     wlimit = wzong.actions_all["warnthreshold"]
     slimit = wzong.actions_all["stopthreshold"]
-    mmean = match_allv.mean()
+    mmean = np.nanmean(match_allv)
 
-    well_all.append("SUM_WELLS")
-    match_all.append(mmean)
-    well_warn_limit.append(wlimit)
-    well_stop_limit.append(slimit)
+    results["WELL"].append("SUM_WELLS")
+    results["MATCH"].append(mmean)
+    results["WARN_LIMIT"].append(wlimit)
+    results["STOP_LIMIT"].append(wlimit)
 
     status = "OK"
     if mmean < wlimit:
@@ -175,18 +229,9 @@ def wellzonation_vs_grid(
     if mmean < slimit:
         status = "STOP"
 
-    well_status.append(status)
+    results["MATCH"].append(status)
 
-    dfr = _make_report(
-        self,
-        wzong,
-        data,
-        well_all,
-        match_all,
-        well_warn_limit,
-        well_stop_limit,
-        well_status,
-    )
+    dfr = _make_report(self, wzong, results,)
 
     self.print_debug("Results:")
     print(dfr)
@@ -200,3 +245,5 @@ def wellzonation_vs_grid(
         print(dfr_stop, file=sys.stderr)
         msg = "One or more wells has status = STOP"
         self.force_stop(msg)
+
+    return dfr
--- tests/test_qcforward/test_wellzon_vs_grid.py
+++ tests/test_qcforward/test_wellzon_vs_grid.py
@@ -21,6 +21,7 @@ WELLFILES = [
 ]
 
 ZONELOGNAME = "Zonelog"
+PERFLOGNAME = "Perflog"
 REPORT = abspath("./somefile.csv")
 
 
@@ -34,8 +35,7 @@ def test_zonelog_vs_grid_asfiles():
         "grid": GRIDFILE,
         "zone": {ZONENAME: ZONEFILE},
         "wells": WELLFILES,
-        "zonelogname": ZONELOGNAME,
-        "zonelogrange": [1, 3],  # inclusive range at both ends
+        "zonelog": {"name": ZONELOGNAME, "range": [1, 3]},
         "depthrange": [1580, 9999],
         "actions_each": {"warnthreshold": 50, "stopthreshold": 20},
         "actions_all": {"warnthreshold": 80, "stopthreshold": 20},
@@ -82,3 +82,41 @@ def test_zonelog_vs_grid_asfiles_shall_stop():
 
     with pytest.raises(SystemExit):
         wellcheck.wellzonation_vs_grid(data)
+
+
+@pytest.mark.skipif(sys.version_info < (3, 0), reason="requires Python3")
+def test_perflog_vs_grid_asfiles():
+    """
+    Testing the perforation log as zonelog filter vs grid functionality using files.
+
+    When a perflog is present, it means in this example
+    that all intervals within a PERFLOG range will have a zonelog vs grid check,
+    i.e. the PERFLOG acts a contrain/filter. In intervals with missing PERFLOG,
+    or if PERFLOG is zero, then zonelog vs grid checks is ignored.
+
+
+    """
+
+    data = {
+        "verbosity": "debug",
+        "path": PATH,
+        "grid": GRIDFILE,
+        "zone": {ZONENAME: ZONEFILE},
+        "wells": WELLFILES,
+        "zonelogname": ZONELOGNAME,
+        "zonelogrange": [1, 3],  # inclusive range at both ends
+        "depthrange": [1580, 9999],
+        "perforationlog": {"name": PERFLOGNAME, "range": [1, 9999]},
+        "actions_each": {"warnthreshold": 50, "stopthreshold": 20},
+        "actions_all": {"warnthreshold": 80, "stopthreshold": 20},
+        "report": {"file": REPORT, "mode": "write"},
+        "dump_yaml": "somefile.yml",
+    }
+
+    wellcheck = qcf.QCForward()
+    # wellcheck.wellzonation_vs_grid(data)
+
+    # print(dfr)
+    # assert dfr.loc[11, "MATCH"] == pytest.approx(58.15, 0.01)
+    # os.unlink("somefile.yml")
+    # os.unlink(REPORT)
