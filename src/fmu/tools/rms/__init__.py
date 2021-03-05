6293a208f88f1ee9c4141b427ecae1af786bd065
Jimmy Zurcher <jiz@equinor.com>
2021-03-05 17:59:51 +0100

    Add set_data_empty method to qc reset and edit init file

--- src/fmu/tools/rms/__init__.py
+++ src/fmu/tools/rms/__init__.py
@@ -3,5 +3,6 @@ Processing of volumetrics from RMS
 """
 
 from .volumetrics import rmsvolumetrics_txt2df
+from .qcreset import set_data_constant, set_data_empty
 
-__all__ = ["rmsvolumetrics_txt2df"]
+__all__ = ["rmsvolumetrics_txt2df", "set_data_constant", "set_data_empty"]
