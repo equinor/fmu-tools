15927ae584138ff9dc88070160aa7e875c24637f
Jimmy Zurcher <jiz@equinor.com>
2021-03-08 11:59:59 +0100

    reset init file as before

--- src/fmu/tools/rms/__init__.py
+++ src/fmu/tools/rms/__init__.py
@@ -3,6 +3,5 @@ Processing of volumetrics from RMS
 """
 
 from .volumetrics import rmsvolumetrics_txt2df
-from .qcreset import set_data_constant, set_data_empty
 
-__all__ = ["rmsvolumetrics_txt2df", "set_data_constant", "set_data_empty"]
+__all__ = ["rmsvolumetrics_txt2df"]
