66dd3df74f78c71806207f5f0a7bbb559b067acb
Jan C. Rivenæs <jan@rivenaes.net>
2021-03-17 07:51:32 +0100

    Create RMS project in testing

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import os
+import pytest
+
+
+def pytest_runtest_setup(item):
+    """Called for each test."""
+
+    markers = [value.name for value in item.iter_markers()]
+
+    # pytest.mark.skipifroxar:
+    if "skipifroxar" in markers:
+        if "ROXENV" in os.environ:
+            pytest.skip("Skip test in ROXENV (env variable ROXENV is present)")
+
+    # pytest.mark.skipunlessroxar:
+    if "skipunlessroxar" in markers:
+        if "ROXENV" not in os.environ:
+            pytest.skip("Skip test if outside ROXENV (env variable ROXENV is present)")
