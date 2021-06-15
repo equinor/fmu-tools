8b1c199184c6c724838990c16369ac00b3f88d4b
Håvard Berland <havb@equinor.com>
2021-06-15 10:17:24 +0200

    Add disjoint-sets feature to fipmapper (#135)

--- src/fmu/tools/fipmapper/fipmapper.py
+++ src/fmu/tools/fipmapper/fipmapper.py
@@ -2,9 +2,13 @@
 
 from pathlib import Path
 import logging
-from typing import Union, Dict, List, Any, Optional
+import itertools
+import collections
+from typing import Union, Dict, List, Any, Optional, Tuple
 
 import yaml
+import pandas as pd
+from disjoint_set import DisjointSet
 
 logger = logging.getLogger(__name__)
 
@@ -12,6 +16,7 @@ logger = logging.getLogger(__name__)
 class FipMapper:
     def __init__(
         self,
+        *,
         yamlfile: Union[str, Path] = None,
         mapdata: Dict[str, str] = None,
         skipstring: Union[List[str], str] = None,
@@ -167,6 +172,21 @@ class FipMapper:
     def _fips2regions(self, fips: List[int]) -> List[List[str]]:
         return [self.fip2region(fip_int) for fip_int in fips]
 
+    def get_regions(self) -> List[str]:
+        """Obtain a sorted list of the regions that exist in the map"""
+        assert "region2fipnum" in self._mapdata, "No data provided for regions"
+        return sorted(self._mapdata["region2fipnum"].keys())
+
+    def get_zones(self) -> List[str]:
+        """Obtain a sorted list of the zones that exist in the map"""
+        assert "zone2fipnum" in self._mapdata, "No data provided for regions"
+        return sorted(self._mapdata["zone2fipnum"].keys())
+
+    def get_fipnums(self) -> List[str]:
+        """Obtain a sorted list of the fip numbers that exist in the map"""
+        assert "fipnum2region" in self._mapdata, "No data provided for regions"
+        return sorted(self._mapdata["fipnum2region"].keys())
+
     def fip2region(self, fip: Optional[int]) -> List[str]:
         """Maps one FIP(NUM) integer to list of Region strings. Each FIPNUM
         can map to multiple regions, therefore a list is always returned for
@@ -270,6 +290,178 @@ class FipMapper:
         fipzon = self.zone2fip(zone)
         return sorted(list(set(fipreg).intersection(set(fipzon))))
 
+    def disjoint_sets(self) -> pd.DataFrame:
+        """Determine the minimal disjoint sets of a reservoir
+
+        The disjoint sets returned consist of sets that can be split into
+        both a set of FIPxxxx list and a region/zone list. Thus, the sum of
+        any additive property is comparable on these disjoint sets.
+
+        The returned object is a dataframe that is to be used to group together
+        fipnums or regions/zones so they are summable.
+
+        Each row represents a cell in the partition where both region, zone and
+        fipnum boundaries apply, this the finest possible partition the
+        fipmapper data allows. Each row is then assigned to a integer
+        identifier in the ``SET`` column. The chosen integers values for each
+        set is based on lexiographical sorting of regions, zones and fipnum
+        values.
+
+        These sets signifies the minimal grouping of data that must be applied
+        in order for volumes in the region/zone partition or fipnum partition
+        to be comparable.
+        """
+
+        # Generate all possible combinations of the regions and
+        # zones we know of:
+        regzone_df = pd.DataFrame(
+            columns=["REGION", "ZONE"],
+            data=itertools.product(self.get_regions(), self.get_zones()),
+        )
+
+        # Map all of the region-zone combinations into the accompanying FIPNUMs:
+        regzone_df["FIPNUMS"] = regzone_df.apply(
+            lambda x: self.regzone2fip(x["REGION"], x["ZONE"]), axis=1
+        )
+
+        # The dataframe has lists in the FIPNUMS column when a reg/zone maps
+        # to multiple FIPNUMs. Unroll these into one row pr linked FIPNUM:
+        dframe = _expand_regzone_df(regzone_df)
+
+        # The `dframe` now has one row pr. smallest "cell" that is interesting
+        # in the current context. In some sense, the "intersection" of all
+        # possible partitions.
+
+        # Create a dataframe of all possible combinations of these smallest cells:
+        edges = pd.merge(
+            dframe.assign(dummy=1), dframe.assign(dummy=1), on="dummy"
+        ).drop("dummy", axis=1)
+        # When Pandas 1.2 is ubiqutous, replace the above statement with:
+        # edges = dframe.merge(dframe, how="cross")
+
+        # A partition is equivalent to an equivalence relation.
+
+        # Apply an equivalence relation to the cell combinations:
+        edges["NEIGHBOURS"] = edges.apply(
+            lambda x: _equivalent_cells(
+                x["REGION_x"],
+                x["ZONE_x"],
+                x["FIPNUM_x"],
+                x["REGION_y"],
+                x["ZONE_y"],
+                x["FIPNUM_y"],
+            ),
+            axis=1,
+        )
+        # Filter to only edges that determine which cell linkages
+        # that should be grouped:
+        neighbourlist = edges[edges["NEIGHBOURS"]]
+
+        # Construct a disjoint set object of all the smallest cells that are
+        # to be grouped/unionized:
+        ds: DisjointSet = DisjointSet()
+        for _, row in dframe.iterrows():
+            ds.find((row["REGION"], row["ZONE"], row["FIPNUM"]))
+
+        # Apply the union-find algorithm to determine the partition
+        # where all equivalene relations are obeyed:
+        for _, pair in neighbourlist.iterrows():
+            ds.union(
+                (pair["REGION_x"], pair["ZONE_x"], pair["FIPNUM_x"]),
+                (pair["REGION_y"], pair["ZONE_y"], pair["FIPNUM_y"]),
+            )
+
+        # The union-find algorithm has now "named" each of the components
+        # in the disjoint set by a somewhat random mother/root node. This root
+        # not is not any more a root compared to the other cells in the set,
+        # so each set is instead mapped to consecutive integers.
+        id_dict: dict = collections.defaultdict(lambda: len(id_dict))
+        dframe["SET"] = [
+            id_dict[root]
+            for root in dframe.sort_values(["REGION", "ZONE", "FIPNUM"]).apply(
+                lambda x: ds.find((x["REGION"], x["ZONE"], x["FIPNUM"])), axis=1
+            )
+        ]
+        return dframe
+
+
+def _equivalent_cells(
+    reg1: Any, zon1: Any, fip1: Any, reg2: Any, zon2: Any, fip2: Any
+) -> bool:
+    """Define the equivalence relation for the reg-zone-fip reservoir partition
+
+    A pair of reg-zone-fip is in the same group if they must be treated together
+    when (and have properties summed). Say if you have a value for
+    a specific region, but this region contains two FIPNUMs. Then we can never
+    treat these two FIPNUMs separately, they must be summed in order to be
+    comparable to the value for the region
+    """
+    return ((reg1 == reg2) and (zon1 == zon2)) or (fip1 == fip2)
+
+
+def regions_in_set(dframe: pd.DataFrame) -> Dict[int, List[str]]:
+    """From the dataframe returned by disjoint_sets(), compute
+    a dictionary to map from a set index to a list of regions
+    that are members of that set index
+
+    Args:
+        dframe: The dataframe emitted by disjoint_sets()
+    """
+    if dframe.empty:
+        return {}
+    return (
+        dframe.groupby("SET")["REGION"].apply(set).apply(list).apply(sorted).to_dict()
+    )
+
+
+def zones_in_set(dframe: pd.DataFrame) -> Dict[int, List[str]]:
+    """From the dataframe returned by disjoint_sets(), compute
+    a dictionary to map from a set index to a list of zones
+    that are members of that set index
+
+    Args:
+        dframe: The dataframe emitted by disjoint_sets()
+    """
+    if dframe.empty:
+        return {}
+    return dframe.groupby("SET")["ZONE"].apply(set).apply(list).apply(sorted).to_dict()
+
+
+def fipnums_in_set(dframe: pd.DataFrame) -> Dict[int, List[int]]:
+    """From the dataframe returned by disjoint_sets(), compute
+    a dictionary to map from a set index to a list of FIPNUM values
+    that are members of that set index
+
+    Args:
+        dframe: The dataframe emitted by disjoint_sets()
+    """
+    if dframe.empty:
+        return {}
+    return (
+        dframe.groupby("SET")["FIPNUM"].apply(set).apply(list).apply(sorted).to_dict()
+    )
+
+
+def regzonefips_in_set(dframe: pd.DataFrame) -> Dict[int, List[Tuple[str, str, int]]]:
+    """From the dataframe returned by disjoint_sets(), compute
+    a dictionary to map from a set index to a list of tuples
+    of the region, zones and fipnums in the set.
+
+    Args:
+        dframe: The dataframe emitted by disjoint_sets()
+    """
+    if dframe.empty:
+        return {}
+    dframe = dframe.copy()
+    dframe["reg-zone-fip"] = dframe[["REGION", "ZONE", "FIPNUM"]].apply(tuple, axis=1)
+    return (
+        dframe.groupby("SET")["reg-zone-fip"]
+        .apply(set)
+        .apply(list)
+        .apply(sorted)
+        .to_dict()
+    )
+
 
 def webviz_to_prtvol2csv(webvizdict: dict):
     """Convert a dict representation of a region/zone map in the Webviz format
@@ -323,3 +515,19 @@ def invert_map(
         inv_map[key] = sorted(list(inv_map[key]))
 
     return inv_map
+
+
+def _expand_regzone_df(dframe: pd.DataFrame, fipname: str = "FIPNUM") -> pd.DataFrame:
+    """Unroll dataframe rows with a FIPNUM list in the "FIPNUMS" column"""
+    new_rows = []
+    for _, row in dframe.iterrows():
+        for fipnumber in row[fipname + "S"]:
+            new_rows.append(
+                {
+                    "REGION": row["REGION"],
+                    "ZONE": row["ZONE"],
+                    fipname: fipnumber,
+                    "REGZONE": row["REGION"] + "-" + row["ZONE"],
+                }
+            )
+    return pd.DataFrame(new_rows)
---
+++ tests/fipmapper/test_disjointsets.py
@@ -0,0 +1,287 @@
+from packaging import version
+
+import pytest
+
+from fmu.tools.fipmapper import fipmapper
+
+import pandas as pd
+
+
+@pytest.mark.skipif(
+    version.parse(pd.__version__) < version.parse("0.25.2"),
+    reason="Pandas 0.25.2 is required for fipmappers disjointsets()",
+)
+@pytest.mark.parametrize(
+    "mapdata, expected_disjointsets",
+    [
+        (
+            # The first test proves the code on a three-by-three reservoir model
+            # which includes the major "cruxes" to be solved. This serves
+            # as both the motivation for the code and as demonstration of
+            # the solution.
+            #
+            # In the geomodel, the reservoir is three-by-three
+            # with region-zone pairs as follows:
+            #
+            #    +--------+--------+--------+
+            #    | (A, U) | (B, U) | (C, U) |
+            #    +--------+--------+--------+
+            #    | (A, M) | (B, M) | (C, M) |
+            #    +--------+--------+--------+
+            #    | (A, L) | (B, L) | (C, L) |
+            #    +--------+--------+--------+
+            #
+            # while in the dynamical (Eclipse) model the reservoir has
+            # a FIPNUM split like this:
+            #
+            #  regs: A    |   B     |    C       zones:
+            #     +-----------------+----+----+
+            #     |                 |  2 |  3 |    U
+            #     |       1         +----+----+
+            #     |                 |         |    M
+            #     +-----------------+    4    |
+            #     |       5         |         |    L
+            #     +-----------------+---------+
+            #
+            # The problem the code solves is to determine which of these cells
+            # that must be joined in order to have comparable numbers (when say
+            # volume is available for each cell in the geomodel and dynamical
+            # model separately.
+            #
+            # In this case, the joined structure must be a 4-cell partition:
+            #
+            #     +-----------------+---------+
+            #     |                 |    3    |
+            #     |       1         +---------+
+            #     |                 |         |
+            #     +-----------------+    2    |
+            #     |       0         |         |
+            #     +-----------------+---------+
+            #
+            # where the code identifies each set of joined cells by a
+            # unique integer from a consecutive list starting at zero.
+            # The enumeration of sets are predictable, based on lexiographical
+            # sorts of REGION, ZONE and FIPNUM.
+            {
+                # The input is just mappings from the (region, zone)
+                # "tuple" in the geomodel into FIPNUM in the dynamical
+                # model. The spatial layout of regions, zones and FIPNUM
+                # as displayed above does not matter.
+                "region2fipnum": {
+                    "A": [1, 5],
+                    "B": [1, 5],
+                    "C": [2, 3, 4],
+                },
+                "zone2fipnum": {
+                    "U": [1, 2, 3],
+                    "M": [1, 4],
+                    "L": [5, 4],
+                },
+            },
+            pd.DataFrame(
+                columns=["REGION", "ZONE", "FIPNUM", "SET"],
+                data=[
+                    ["A", "L", 5, 0],
+                    ["A", "M", 1, 1],
+                    ["A", "U", 1, 1],
+                    ["B", "L", 5, 0],
+                    ["B", "M", 1, 1],
+                    ["B", "U", 1, 1],
+                    ["C", "L", 4, 2],
+                    ["C", "M", 4, 2],
+                    ["C", "U", 2, 3],
+                    ["C", "U", 3, 3],
+                ],
+            ),
+        ),
+        pytest.param({}, [], marks=pytest.mark.xfail(raises=AssertionError)),
+        (
+            # Only one cell in both regzone partition and fipnum partition:
+            {"region2fipnum": {"A": 1}, "zone2fipnum": {"U": 1}},
+            [{"REGION": "A", "ZONE": "U", "FIPNUM": 1, "SET": 0}],
+        ),
+        (
+            # Same as above, but test that we can provide it as list input:
+            {"region2fipnum": {"A": [1]}, "zone2fipnum": {"U": [1]}},
+            [{"REGION": "A", "ZONE": "U", "FIPNUM": 1, "SET": 0}],
+        ),
+        (
+            # FIPNUM split in two, gives only one group in return:
+            {"region2fipnum": {"A": [1, 2]}, "zone2fipnum": {"U": [1, 2]}},
+            [
+                {"REGION": "A", "ZONE": "U", "FIPNUM": 1, "SET": 0},
+                {"REGION": "A", "ZONE": "U", "FIPNUM": 2, "SET": 0},
+            ],
+        ),
+        (
+            # FIPNUM split in two, and two zones, gives two groups in return:
+            {"region2fipnum": {"A": [1, 2]}, "zone2fipnum": {"U": [1], "L": [2]}},
+            [
+                {"REGION": "A", "ZONE": "L", "FIPNUM": 2, "SET": 0},
+                {"REGION": "A", "ZONE": "U", "FIPNUM": 1, "SET": 1},
+            ],
+        ),
+        (
+            # Two zones, one FIPNUM, gives one group in return:
+            {"region2fipnum": {"A": 1}, "zone2fipnum": {"U": [1], "L": [1]}},
+            [
+                {"REGION": "A", "ZONE": "L", "FIPNUM": 1, "SET": 0},
+                {"REGION": "A", "ZONE": "U", "FIPNUM": 1, "SET": 0},
+            ],
+        ),
+        (
+            # Two regions, one FIPNUM, gives one group in return:
+            {"region2fipnum": {"A": 1, "B": 1}, "zone2fipnum": {"U": 1}},
+            [
+                {"REGION": "A", "ZONE": "U", "FIPNUM": 1, "SET": 0},
+                {"REGION": "B", "ZONE": "U", "FIPNUM": 1, "SET": 0},
+            ],
+        ),
+        (
+            # Two regions, two zones, 4 FIPNUMs -> 4 groups:
+            {
+                "region2fipnum": {"A": [2, 1], "B": [3, 4]},
+                "zone2fipnum": {"U": [1, 3], "L": [2, 4]},
+            },
+            [
+                {"REGION": "A", "ZONE": "L", "FIPNUM": 2, "SET": 0},
+                {"REGION": "A", "ZONE": "U", "FIPNUM": 1, "SET": 1},
+                {"REGION": "B", "ZONE": "L", "FIPNUM": 4, "SET": 2},
+                {"REGION": "B", "ZONE": "U", "FIPNUM": 3, "SET": 3},
+            ],
+        ),
+        (
+            # Two regions, two zones, 2 horizontal FIPNUMs -> 2 groups:
+            {
+                "region2fipnum": {"A": [2, 1], "B": [1, 2]},
+                "zone2fipnum": {"U": 1, "L": [2, 2]},  # Double for L is ignored.
+            },
+            [
+                {"REGION": "A", "ZONE": "L", "FIPNUM": 2, "SET": 0},
+                {"REGION": "A", "ZONE": "U", "FIPNUM": 1, "SET": 1},
+                {"REGION": "B", "ZONE": "L", "FIPNUM": 2, "SET": 0},
+                {"REGION": "B", "ZONE": "U", "FIPNUM": 1, "SET": 1},
+            ],
+        ),
+        (
+            # Two regions, two zones, 2 region-wise FIPNUMs -> 2 groups:
+            {
+                "region2fipnum": {"A": 1, "B": 2},
+                "zone2fipnum": {"U": [1, 2], "L": [1, 2]},
+            },
+            [
+                {"REGION": "A", "ZONE": "L", "FIPNUM": 1, "SET": 0},
+                {"REGION": "A", "ZONE": "U", "FIPNUM": 1, "SET": 0},
+                {"REGION": "B", "ZONE": "L", "FIPNUM": 2, "SET": 1},
+                {"REGION": "B", "ZONE": "U", "FIPNUM": 2, "SET": 1},
+            ],
+        ),
+        (
+            # Two regions, one zone, 2 horizontal FIPNUMs which don't align
+            # with the region boundary -> 1 group:
+            {
+                "region2fipnum": {"A": [1], "B": [1, 2]},
+                "zone2fipnum": {"U": [1, 2]},
+            },
+            [
+                {"REGION": "A", "ZONE": "U", "FIPNUM": 1, "SET": 0},
+                {"REGION": "B", "ZONE": "U", "FIPNUM": 1, "SET": 0},
+                {"REGION": "B", "ZONE": "U", "FIPNUM": 2, "SET": 0},
+            ],
+        ),
+    ],
+)
+def test_disjointsets(mapdata, expected_disjointsets):
+    expected_dframe = pd.DataFrame(expected_disjointsets)
+    cols = list(expected_dframe.columns)
+    mapper = fipmapper.FipMapper(mapdata=mapdata)
+    pd.testing.assert_frame_equal(
+        mapper.disjoint_sets()[cols].sort_values(by=cols, axis=0),
+        expected_dframe.sort_values(by=cols, axis=0),
+    )
+
+
+@pytest.mark.parametrize(
+    "dframe_records, expected_regions, expected_zones, "
+    "expected_fipnums, expected_regzonefips",
+    [
+        ([{}], {}, {}, {}, {}),
+        (
+            [{"REGION": "A", "ZONE": "U", "FIPNUM": 1, "SET": 0}],
+            {0: ["A"]},
+            {0: ["U"]},
+            {0: [1]},
+            {0: [("A", "U", 1)]},
+        ),
+        (
+            [
+                {"REGION": "A", "ZONE": "U", "FIPNUM": 1, "SET": 0},
+                {"REGION": "B", "ZONE": "U", "FIPNUM": 1, "SET": 0},
+            ],
+            {0: ["A", "B"]},
+            {0: ["U"]},
+            {0: [1]},
+            {0: [("A", "U", 1), ("B", "U", 1)]},
+        ),
+        (
+            [
+                # Switched order of rows compared to above:
+                {"REGION": "B", "ZONE": "U", "FIPNUM": 1, "SET": 0},
+                {"REGION": "A", "ZONE": "U", "FIPNUM": 1, "SET": 0},
+            ],
+            {0: ["A", "B"]},
+            {0: ["U"]},
+            {0: [1]},
+            {0: [("A", "U", 1), ("B", "U", 1)]},
+        ),
+        (
+            [
+                {"REGION": "A", "ZONE": "U", "FIPNUM": 1, "SET": 0},
+                {"REGION": "B", "ZONE": "U", "FIPNUM": 2, "SET": 1},
+            ],
+            {0: ["A"], 1: ["B"]},
+            {0: ["U"], 1: ["U"]},
+            {0: [1], 1: [2]},
+            {0: [("A", "U", 1)], 1: [("B", "U", 2)]},
+        ),
+        (
+            # The full example from the top of this file:
+            pd.DataFrame(
+                columns=["REGION", "ZONE", "FIPNUM", "SET"],
+                data=[
+                    ["A", "L", 5, 0],
+                    ["A", "M", 1, 1],
+                    ["A", "U", 1, 1],
+                    ["B", "L", 5, 0],
+                    ["B", "M", 1, 1],
+                    ["B", "U", 1, 1],
+                    ["C", "L", 4, 2],
+                    ["C", "M", 4, 2],
+                    ["C", "U", 2, 3],
+                    ["C", "U", 3, 3],
+                ],
+            ).to_dict(orient="records"),
+            {0: ["A", "B"], 1: ["A", "B"], 2: ["C"], 3: ["C"]},
+            {0: ["L"], 1: ["M", "U"], 2: ["L", "M"], 3: ["U"]},
+            {0: [5], 1: [1], 2: [4], 3: [2, 3]},
+            {
+                0: [("A", "L", 5), ("B", "L", 5)],
+                1: [("A", "M", 1), ("A", "U", 1), ("B", "M", 1), ("B", "U", 1)],
+                2: [("C", "L", 4), ("C", "M", 4)],
+                3: [("C", "U", 2), ("C", "U", 3)],
+            },
+        ),
+    ],
+)
+def test_set_lookups(
+    dframe_records,
+    expected_regions,
+    expected_zones,
+    expected_fipnums,
+    expected_regzonefips,
+):
+    dframe = pd.DataFrame(dframe_records)
+    assert fipmapper.regions_in_set(dframe) == expected_regions
+    assert fipmapper.zones_in_set(dframe) == expected_zones
+    assert fipmapper.fipnums_in_set(dframe) == expected_fipnums
+    assert fipmapper.regzonefips_in_set(dframe) == expected_regzonefips
