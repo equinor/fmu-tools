71f4c33782ce05c4917ef11f84d7189bdc31bdda
fmu-tools-github-action <fmu-tools-github-action>
2021-07-01 10:04:24 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.8.0rc1.dev3+g862fd81',
+    VERSION: '1.8.0rc1.dev4+ga6328f1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
