814f1b9f00dac591df5d2c48d3e4f0b79d94c488
Jan C. Rivenæs <jan@rivenaes.net>
2023-01-13 15:46:36 +0100

    Fixup (2)

--- src/fmu/tools/rms/__init__.py
+++ src/fmu/tools/rms/__init__.py
@@ -2,8 +2,7 @@
 Processing of volumetrics from RMS
 """
 
-from .volumetrics import rmsvolumetrics_txt2df
 from .import_localmodule import import_localmodule
+from .volumetrics import rmsvolumetrics_txt2df
 
-
-__all__ = ["rmsvolumetrics_txt2df"]
+__all__ = ["rmsvolumetrics_txt2df", "import_localmodule"]
