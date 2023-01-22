0385742223d67207ca4cdb6526dfd39bfa778058
Jan C. Rivenæs <jan@rivenaes.net>
2023-01-22 16:19:25 +0100

    Reintroduce Sphinx documentation in Github actions

--- src/fmu/tools/rms/__init__.py
+++ src/fmu/tools/rms/__init__.py
@@ -2,7 +2,7 @@
 Processing of volumetrics from RMS
 """
 
-from .import_localmodule import import_localmodule
+from .import_localmodules import import_localmodule
 from .volumetrics import rmsvolumetrics_txt2df
 
 __all__ = ["rmsvolumetrics_txt2df", "import_localmodule"]
