ec4a31533b7b9f3eec75e89b9c559cf759ec9099
Oddvar Lia (ST MSU GEO) <olia@equinor.com>
2024-12-11 10:46:01 +0100

    Add function to combine multiple petro realizations using facies realization
    Add function to import and export field parameters to and from ERTBOX grid.
    Added documentation for update_petro_real and the export and import functions.
    Add test for update_petro_real.py

--- src/fmu/tools/rms/__init__.py
+++ src/fmu/tools/rms/__init__.py
@@ -7,6 +7,11 @@ from .generate_petro_jobs_for_field_update import (
     main as generate_petro_jobs,
 )
 from .import_localmodules import import_localmodule
+from .update_petro_real import (
+    export_initial_field_parameters,
+    import_updated_field_parameters,
+    update_petro_real as update_petro_parameters,
+)
 from .volumetrics import rmsvolumetrics_txt2df
 
 __all__ = [
@@ -14,4 +19,7 @@ __all__ = [
     "import_localmodule",
     "generate_petro_jobs",
     "create_bw_per_facies",
+    "update_petro_parameters",
+    "import_updated_field_parameters",
+    "export_initial_field_parameters",
 ]
