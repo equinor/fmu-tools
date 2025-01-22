fd674d25793a93697186833b93ab4363be76ec7f
Sindre Langeveld <slang@equinor.com>
2025-01-22 15:50:44 +0100

    CI: Clone xtgeo testdata for tests

--- ci/testrmsenv.sh
+++ ci/testrmsenv.sh
@@ -1,29 +1,47 @@
 # This shell script is to be sourced and run from a github workflow
-# when fmu-tools is to be tested towards a new RMS enviroment
+# when fmu-tools is to be tested towards a new RMS Python enviroment
 
 run_tests () {
+    set_test_variables
+
     copy_test_files
 
     install_test_dependencies
 
-    pushd $CI_TEST_ROOT
-    start_tests
-    popd
+    run_pytest
+}
+
+set_test_variables() {
+    echo "Setting variables for xtgeo tests..."
+    CI_TEST_ROOT=$CI_ROOT/fmutools-test-root
+}
+
+copy_test_files () {
+    echo "Copy xtgeo testdata for xtgeo related tests..."
+    if ! [ -d $XTGEO_TESTDATA_PATH ]; then
+        echo "Cloning xtgeo-testdata to $XTGEO_TESTDATA_PATH..."
+        git clone --depth=1 https://github.com/equinor/xtgeo-testdata $XTGEO_TESTDATA_PATH
+    else
+        echo "xtgeo testdata already exists at path $XTGEO_TESTDATA_PATH. Skipping copy."
+    fi
+
+    echo "Copy fmu-tools test files to test folder $CI_TEST_ROOT..."
+    mkdir -p $CI_TEST_ROOT  
+    cp -r $PROJECT_ROOT/tests $CI_TEST_ROOT/tests
+    cp $PROJECT_ROOT/pyproject.toml $CI_TEST_ROOT
 }
 
 install_test_dependencies () {
     echo "Installing test dependencies..."
     pip install ".[tests]"
+
+    echo "Dependencies installed successfully. Listing installed dependencies..."
+    pip list
 }
 
-copy_test_files () {
-    echo "Copy test files to test folder $CI_TEST_ROOT..."
-    cp -r $CI_SOURCE_ROOT/tests $CI_TEST_ROOT/tests
-    # Pytest configuration is in pyproject.toml
-    cp $CI_SOURCE_ROOT/pyproject.toml $CI_TEST_ROOT
-}
-
-start_tests () {
+run_pytest () {
     echo "Running fmu-tools tests with pytest..."
-    pytest
+    pushd $CI_TEST_ROOT
+    pytest -n 4 -vv -m "not skipunlessroxar"
+    popd
 }
