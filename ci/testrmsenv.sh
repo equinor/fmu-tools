7e399562a0e5899cac36c980883fa03f912c0d39
Sindre Langeveld <slang@equinor.com>
2025-01-24 16:34:50 +0100

    CI: Use xtgeo-testdata from cache

--- ci/testrmsenv.sh
+++ ci/testrmsenv.sh
@@ -17,17 +17,9 @@ set_test_variables() {
 }
 
 copy_test_files () {
-    echo "Copy xtgeo testdata for xtgeo related tests..."
-    if ! [ -d $XTGEO_TESTDATA_PATH ]; then
-        echo "Cloning xtgeo-testdata to $XTGEO_TESTDATA_PATH..."
-        git clone --depth=1 https://github.com/equinor/xtgeo-testdata $XTGEO_TESTDATA_PATH
-    else
-        echo "xtgeo testdata already exists at path $XTGEO_TESTDATA_PATH. Skipping copy."
-    fi
-
     echo "Copy fmu-tools test files to test folder $CI_TEST_ROOT..."
     mkdir -p $CI_TEST_ROOT  
-    cp -r $PROJECT_ROOT/tests $CI_TEST_ROOT/tests
+    cp -r $PROJECT_ROOT/tests $CI_TEST_ROOT
     cp $PROJECT_ROOT/pyproject.toml $CI_TEST_ROOT
 }
 
@@ -42,6 +34,6 @@ install_test_dependencies () {
 run_pytest () {
     echo "Running fmu-tools tests with pytest..."
     pushd $CI_TEST_ROOT
-    pytest -n 4 -vv
+    pytest ./tests -n 4 -vv --testdatapath $XTGEO_TESTDATA_PATH
     popd
 }
