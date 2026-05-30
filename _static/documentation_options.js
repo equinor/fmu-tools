8c34c8beaff3f0c036a8d370fec1b4c751e761cb
fmu-tools-github-action <fmu-tools-github-action>
2026-05-30 00:29:03 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.30.1.dev33+g9bc7ab2d4.d20260529',
+    VERSION: '1.30.1.dev33+g9bc7ab2d4.d20260530',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
